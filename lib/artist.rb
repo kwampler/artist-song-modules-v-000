371a31b3450c5c3b288777dbf63d472a60bd46b1
Kate Preston <katejsenger@yahoo.com>
2018-11-21 22:19:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,6 @@
 require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
+require_relative '../lib/concerns/paramable'
 class Artist
   attr_accessor :name
   attr_reader :songs
